a7979fbd49bfe21febf49550cd58008b5fd9b34f
Antoninstolfa <97514759+Antoninstolfa@users.noreply.github.com>
2022-01-26 22:02:52 +0100

    Add files via upload

---
+++ Spolecny ukol/Spolecny ukol/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿
+namespace Spolecny_ukol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBoxFaktorial = new System.Windows.Forms.CheckBox();
+            this.checkBoxMocnina = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelVysledek = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBoxFaktorial
+            // 
+            this.checkBoxFaktorial.AutoSize = true;
+            this.checkBoxFaktorial.Location = new System.Drawing.Point(282, 66);
+            this.checkBoxFaktorial.Name = "checkBoxFaktorial";
+            this.checkBoxFaktorial.Size = new System.Drawing.Size(37, 17);
+            this.checkBoxFaktorial.TabIndex = 0;
+            this.checkBoxFaktorial.Text = "N!";
+            this.checkBoxFaktorial.UseVisualStyleBackColor = true;
+            this.checkBoxFaktorial.CheckedChanged += new System.EventHandler(this.checkBoxFaktorial_CheckedChanged);
+            // 
+            // checkBoxMocnina
+            // 
+            this.checkBoxMocnina.AutoSize = true;
+            this.checkBoxMocnina.Location = new System.Drawing.Point(282, 40);
+            this.checkBoxMocnina.Name = "checkBoxMocnina";
+            this.checkBoxMocnina.Size = new System.Drawing.Size(59, 17);
+            this.checkBoxMocnina.TabIndex = 1;
+            this.checkBoxMocnina.Text = "A na N";
+            this.checkBoxMocnina.UseVisualStyleBackColor = true;
+            this.checkBoxMocnina.CheckedChanged += new System.EventHandler(this.checkBoxMocnina_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(177, 40);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(74, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(177, 66);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(74, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "N";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(279, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Výsledek:";
+            // 
+            // labelVysledek
+            // 
+            this.labelVysledek.AutoSize = true;
+            this.labelVysledek.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelVysledek.Location = new System.Drawing.Point(356, 106);
+            this.labelVysledek.Name = "labelVysledek";
+            this.labelVysledek.Size = new System.Drawing.Size(0, 18);
+            this.labelVysledek.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(13, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(485, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Zadejte hodnotu A ,N a vyberte zdali chcete vypočítat mocninu nebo faktoriál z N." +
+    "\r\n";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(16, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(35, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "?";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(607, 165);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelVysledek);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBoxMocnina);
+            this.Controls.Add(this.checkBoxFaktorial);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBoxFaktorial;
+        private System.Windows.Forms.CheckBox checkBoxMocnina;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelVysledek;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Spolecny ukol/Spolecny ukol/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spolecny_ukol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Vyberte z dvou políček. Pokud chcete vypočítat mocninu tak zaškrtněte první políčko a pokud faktoriál tak druhé.");
+        }
+
+        private void checkBoxMocnina_CheckedChanged(object sender, EventArgs e)
+        {
+            labelVysledek.Text = " ";
+            int a, n;
+            a = Convert.ToInt32(numericUpDown1.Value);
+            n = Convert.ToInt32(numericUpDown2.Value);
+            long mocnina = a;
+            while (n-1 != 0)
+            {
+                mocnina *= a; 
+                n--;
+            }
+            labelVysledek.Text = mocnina.ToString();
+        }
+
+        private void checkBoxFaktorial_CheckedChanged(object sender, EventArgs e)
+        {
+            labelVysledek.Text = " ";
+            long faktorial=1;
+            int n;   
+            n = Convert.ToInt32(numericUpDown2.Value);
+            if(n>0)
+            {
+                while(n!=0)
+                {
+                    faktorial *= n;
+                    n--;
+                }
+                labelVysledek.Text = faktorial.ToString();
+            }
+            else
+            {
+                labelVysledek.Text = "Zadna hodnota pro vypocet!";
+            }
+        }
+    }
+}
